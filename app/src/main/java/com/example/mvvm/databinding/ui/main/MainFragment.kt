033a59e1c299095487971371f4d1a1a7ab306091
setia <putra.simaremare@gmail.com>
2019-10-23 23:51:41 +0700

    completed utilizing viewModel

--- app/src/main/java/com/example/mvvm/databinding/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvvm/databinding/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.mvvm.databinding.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -26,6 +28,32 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.getCounterMan().observe(this, Observer {
+            tv_counter_man.text = "$it"
+        })
+        btn_add_man.setOnClickListener {
+            viewModel.addCounterMan()
+        }
+        btn_minus_man.setOnClickListener {
+            viewModel.reduceCounterMan()
+        }
+        viewModel.getCounterWoman().observe(this, Observer {
+            tv_counter_woman.text = "$it"
+        })
+        btn_add_woman.setOnClickListener {
+            viewModel.addCounterWoman()
+        }
+        btn_minus_woman.setOnClickListener {
+            viewModel.reduceCounterWoman()
+        }
+        viewModel.getCounterKid().observe(this, Observer {
+            tv_counter_kids.text = "$it"
+        })
+        btn_add_kids.setOnClickListener {
+            viewModel.addCounterKid()
+        }
+        btn_minus_kids.setOnClickListener {
+            viewModel.reduceCounterKid()
+        }
     }
 }
