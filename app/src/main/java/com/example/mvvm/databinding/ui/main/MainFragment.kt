586c04515e6a5dc8db85faea88c1b72d1aebeee5
setia <putra.simaremare@gmail.com>
2019-10-25 02:04:43 +0700

    change to dataBinding and utilize viewModel

--- app/src/main/java/com/example/mvvm/databinding/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvvm/databinding/ui/main/MainFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import com.example.mvvm.databinding.R
+import com.example.mvvm.databinding.databinding.MainFragmentBinding
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
@@ -22,38 +23,10 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        val binding = MainFragmentBinding.inflate(layoutInflater)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.getCounterMan().observe(this, Observer {
-            tv_counter_man.text = "$it"
-        })
-        btn_add_man.setOnClickListener {
-            viewModel.addCounterMan()
-        }
-        btn_minus_man.setOnClickListener {
-            viewModel.reduceCounterMan()
-        }
-        viewModel.getCounterWoman().observe(this, Observer {
-            tv_counter_woman.text = "$it"
-        })
-        btn_add_woman.setOnClickListener {
-            viewModel.addCounterWoman()
-        }
-        btn_minus_woman.setOnClickListener {
-            viewModel.reduceCounterWoman()
-        }
-        viewModel.getCounterKid().observe(this, Observer {
-            tv_counter_kids.text = "$it"
-        })
-        btn_add_kids.setOnClickListener {
-            viewModel.addCounterKid()
-        }
-        btn_minus_kids.setOnClickListener {
-            viewModel.reduceCounterKid()
-        }
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 }
